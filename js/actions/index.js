90ef09f46dbfced2aea2da23bf9cdf901daf47db
Mark Watson <mark.watson.x@gmail.com>
2017-09-15 15:15:20 -0500

    Revert to allDocs implementation due to bug in pouchdb react

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,4 +1,4 @@
-import { shoppingListFactory, shoppingListRepository } from '../db'
+import { db, shoppingListFactory, shoppingListRepository } from '../db'
 
 export const ADD_LIST = 'ADD_LIST';
 export const ADD_ITEM = 'ADD_ITEM';
@@ -10,35 +10,76 @@ export const LOAD_ACTIVE_ITEMS = 'LOAD_ACTIVE_ITEMS';
 export const UPDATE_ITEM_CHECKED = 'UPDATE_ITEM_CHECKED';
 export const UPDATE_NEW_ITEM_TEXT = 'UPDATE_NEW_ITEM_TEXT';
 
+// export function loadLists() {
+//   return dispatch => {
+//     let lists = [];
+//     let listItemPromises = [];
+//     shoppingListRepository.find()
+//       .then((allLists) => {
+//         for (let list of allLists) {
+//           lists.push({listId: list._id, list: list, itemCount: 0, itemCheckedCount: 0, items: []});
+//           listItemPromises.push(shoppingListRepository.findItems({
+//             selector: {
+//               type: 'item',
+//               list: list._id
+//             }
+//           }));
+//         }
+//         return Promise.all(listItemPromises);
+//       })
+//       .then((itemLists) => {
+//         for (let itemList of itemLists) {
+//           const itemArray = itemList.toArray();
+//           for (let item of itemArray) {
+//             for (let list of lists) {
+//               if (item.list == list.listId) {
+//                 list.items.push(item);
+//                 list.itemCount = list.itemCount + 1;
+//                 if (item.checked) {
+//                   list.itemCheckedCount = list.itemCheckedCount + 1;
+//                 }
+//                 break;
+//               }
+//             }
+//           }
+//         }
+//         dispatch({
+//           type: LOAD_LISTS,
+//           payload: lists
+//         });
+//       }).catch((err) => {
+//         // TODO:
+//         console.log(err);
+//       });
+//   };
+// }
+
 export function loadLists() {
   return dispatch => {
     let lists = [];
-    let listItemPromises = [];
-    shoppingListRepository.find()
-      .then((allLists) => {
-        for (let list of allLists) {
-          lists.push({listId: list._id, list: list, itemCount: 0, itemCheckedCount: 0, items: []});
-          listItemPromises.push(shoppingListRepository.findItems({
-            selector: {
-              type: 'item',
-              list: list._id
-            }
-          }));
+    // to load all lists we have to use allDocs due to an issue in pouchdb/React Native:
+    // https://github.com/pouchdb/pouchdb/issues/6584
+    db.allDocs({include_docs: true})
+      .then((result) => {
+        // get lists
+        for (let row of result.rows) {
+          doc = row.doc;
+          if (doc.type && doc.type == 'list') {
+            lists.push({listId: doc._id, list: doc, itemCount: 0, itemCheckedCount: 0, items: []});
+          }
         }
-        return Promise.all(listItemPromises);
-      })
-      .then((itemLists) => {
-        for (let itemList of itemLists) {
-          const itemArray = itemList.toArray();
-          for (let item of itemArray) {
+        // get items
+        for (let row of result.rows) {
+          doc = row.doc;
+          if (doc.type && doc.type == 'item') {
             for (let list of lists) {
-              if (item.list == list.listId) {
-                list.items.push(item);
+              if (doc.list == list.listId) {
+                list.items.push(doc);
                 list.itemCount = list.itemCount + 1;
-                if (item.checked) {
+                if (doc.checked) {
                   list.itemCheckedCount = list.itemCheckedCount + 1;
                 }
-                break;
+                break;  
               }
             }
           }
@@ -90,16 +131,20 @@ export function deleteList(list) {
 
 export function loadActiveItems(listId) {
   return dispatch => {
-    shoppingListRepository.findItems({
-      selector: {
-        type: 'item',
-        list: listId
-      }
-    })
-      .then((items) => {
+    // to load active items we have to use allDocs due to an issue in pouchdb/React Native:
+    // https://github.com/pouchdb/pouchdb/issues/6584
+    db.allDocs({include_docs: true})
+      .then((result) => {
+        let items = [];
+        for (let row of result.rows) {
+          doc = row.doc;
+          if (doc.type && doc.type == 'item' && doc.list == listId) {
+            items.push(doc);
+          }
+        }
         dispatch({
           type: LOAD_ACTIVE_ITEMS,
-          payload: items.toArray()
+          payload: items
         });
       }).catch((err) => {
         // TODO:
@@ -107,6 +152,26 @@ export function loadActiveItems(listId) {
       });
   };
 }
+
+// export function loadActiveItems(listId) {
+//   return dispatch => {
+//     shoppingListRepository.findItems({
+//       selector: {
+//         type: 'item',
+//         list: listId
+//       }
+//     })
+//       .then((items) => {
+//         dispatch({
+//           type: LOAD_ACTIVE_ITEMS,
+//           payload: items.toArray()
+//         });
+//       }).catch((err) => {
+//         // TODO:
+//         console.log(err);
+//       });
+//   };
+// }
 
 export function updateNewItemText(text) {
   return {
